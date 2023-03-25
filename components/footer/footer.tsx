9157189aee76e8ec91392cf7818e7257a8080712
panisara-bd <panisara.bnn@gmail.com>
2023-03-25 11:39:00 +0100

    Update footer year

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,15 +1,18 @@
 import styles from "../footer/footer.module.css"
 
 export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+  
   return (
     <footer className={styles.footer}>
       <hr />
       <ul className={styles.navItems}>
         <li className={styles.navItem}>
-          <em>Contact us</em>
+         <a target="_blank" href="https://github.com/plan-b-projects/cv-creator">GitHub</a>
         </li>
         <li className={styles.navItem}>
-          <em>2023</em>
+          <em>&copy; {year} &lt;/salt&gt;</em>
         </li>
       </ul>
     </footer>
