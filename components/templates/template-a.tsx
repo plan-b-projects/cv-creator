a3a06a064057bdc7e096bab304b73d40044b7c77
AlexanderTarler <saltdev@dev-salts-MBP>
2023-03-28 13:12:29 +0200

    Added profile picture to the cv template and made the LinkedIn-link actually go to LinkedIn

--- components/templates/template-a.tsx
+++ components/templates/template-a.tsx
@@ -1,135 +1,120 @@
-import { ReactNode, useEffect, useState } from "react"
-import { CvFormValues } from "../../shared-types"
+import { ReactNode, useEffect, useState } from 'react';
+import { CvFormValues } from '../../shared-types';
 
 export default function TemplateA() {
-    const [data, setData] = useState<CvFormValues>()
+  const [data, setData] = useState<CvFormValues>();
 
-    const getFormValues = async () => {
-        const response = await fetch('http://localhost:3000/api/users/cv-form', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+  const getFormValues = async () => {
+    const response = await fetch('http://localhost:3000/api/users/cv-form', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-        if (response.ok) {
-            const newData = await response.json()
-            return setData(newData)
-        } else {
-            return {}
-        }
+    if (response.ok) {
+      const newData = await response.json();
+      return setData(newData);
+    } else {
+      return {};
     }
+  };
 
-    useEffect(() => {
-        getFormValues()
-    }, [])
+  useEffect(() => {
+    getFormValues();
+  }, []);
 
-    return (
-        <>
-            <div className="basicInfo">
-                <div className="name">{data?.basicInfo.name}</div>
-                <div className="profileIntro">{data?.basicInfo.profileIntro}</div>
-                <div className="location">{data?.basicInfo.location}</div>
-                <div className="email">{data?.basicInfo.email}</div>
-                <div className="tel">{data?.basicInfo.tel}</div>
-                <div className="linkedIn"><a href={data?.basicInfo.location}>LinkedIn</a></div>
-                <div className="github"><a href={data?.basicInfo.gitHub}>GitHub</a></div>
-                <div className="github"><a href={data?.basicInfo.website}>Website</a></div>
+  return (
+    <>
+      <div className="basicInfo">
+        <img src={data?.basicInfo.profilePicture} />
+        <div className="name">{data?.basicInfo.name}</div>
+        <div className="profileIntro">{data?.basicInfo.profileIntro}</div>
+        <div className="location">{data?.basicInfo.location}</div>
+        <div className="email">{data?.basicInfo.email}</div>
+        <div className="tel">{data?.basicInfo.tel}</div>
+        <div className="linkedIn">
+          <a href={data?.basicInfo.linkedIn}>LinkedIn</a>
+        </div>
+        <div className="github">
+          <a href={data?.basicInfo.gitHub}>GitHub</a>
+        </div>
+        <div className="github">
+          <a href={data?.basicInfo.website}>Website</a>
+        </div>
+      </div>
+      <div className="education">
+        {data?.education?.map((edu) => {
+          return (
+            <div>
+              <h3>{edu.course}</h3>
+              <h3>{edu.school}</h3>
+              <h2>{edu.duration}</h2>
+              <p>{edu.description}</p>
             </div>
-            <div className="education">
-                {
-                    data?.education?.map(edu => {
-                        return (
-                            <div>
-                                <h3>{edu.course}</h3>
-                                <h3>{edu.school}</h3>
-                                <h2>{edu.duration}</h2>
-                                <p>{edu.description}</p>
-                            </div>
-                        )
-                    }
-                    )
-                }
+          );
+        })}
+      </div>
+      <div className="experience">
+        {data?.experience?.map((exp) => {
+          return (
+            <div>
+              <h3>{exp.job}</h3>
+              <h3>{exp.company}</h3>
+              <h2>{exp.duration}</h2>
+              <p>{exp.description}</p>
             </div>
-            <div className="experience">
-                {
-                    data?.experience?.map(exp => {
-                        return (
-                            <div>
-                                <h3>{exp.job}</h3>
-                                <h3>{exp.company}</h3>
-                                <h2>{exp.duration}</h2>
-                                <p>{exp.description}</p>
-                            </div>
-                        )
-                    }
-                    )
-                }
+          );
+        })}
+      </div>
+      <div className="skills">
+        <div className="general">
+          {data?.skills?.general?.map((general) => {
+            return (
+              <div>
+                <h3>{general.name}</h3>
+              </div>
+            );
+          })}
+        </div>
+        <div className="frontend">
+          {data?.skills?.frontend?.map((fe) => {
+            return (
+              <div>
+                <h3>{fe.name}</h3>
+              </div>
+            );
+          })}
+        </div>
+        <div className="backend">
+          {data?.skills?.backend?.map((be) => {
+            return (
+              <div>
+                <h3>{be.name}</h3>
+              </div>
+            );
+          })}
+        </div>
+        <div className="tools">
+          {data?.skills?.tools?.map((tool) => {
+            return (
+              <div>
+                <h3>{tool.name}</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="language">
+        {data?.languages?.map((lang) => {
+          return (
+            <div>
+              <h3>{lang.name}</h3>
+              <h3>{lang.fluency}</h3>
             </div>
-            <div className="skills">
-                <div className="general">
-                    {
-                        data?.skills?.general?.map(general => {
-                            return (
-                                <div>
-                                    <h3>{general.name}</h3>
-                                </div>
-                            )
-                        }
-                        )
-                    }
-                </div>
-                <div className="frontend">
-                    {
-                        data?.skills?.frontend?.map(fe => {
-                            return (
-                                <div>
-                                    <h3>{fe.name}</h3>
-                                </div>
-                            )
-                        }
-                        )
-                    }
-                </div>
-                <div className="backend">
-                    {
-                        data?.skills?.backend?.map(be => {
-                            return (
-                                <div>
-                                    <h3>{be.name}</h3>
-                                </div>
-                            )
-                        }
-                        )
-                    }
-                </div>
-                <div className="tools">
-                    {
-                        data?.skills?.tools?.map(tool => {
-                            return (
-                                <div>
-                                    <h3>{tool.name}</h3>
-                                </div>
-                            )
-                        }
-                        )
-                    }
-                </div>
-            </div>
-            <div className="language">
-                {
-                    data?.languages?.map(lang => {
-                        return (
-                            <div>
-                                <h3>{lang.name}</h3>
-                                <h3>{lang.fluency}</h3>
-                            </div>
-                        )
-                    }
-                    )
-                }
-            </div>
-
-        </>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 }
