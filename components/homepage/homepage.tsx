cee718db74ad3b14319d8aa488ba5aba88ca93fe
panisara-bd <panisara.bnn@gmail.com>
2023-04-02 22:42:29 +0200

    Homepage UI

--- components/homepage/homepage.tsx
+++ components/homepage/homepage.tsx
@@ -1,8 +1,8 @@
 import router from 'next/router';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Button } from '../../helpers/button';
-import { H1, H2 } from '../../helpers/theme';
+import { Button, ButtonSecondary } from '../../helpers/button';
+import { H1, H2, H3, colors } from '../../helpers/theme';
 import { CvFormValues } from '../../shared-types';
 import Job, { JobData } from '../job-search/job';
 import { JobsContainer } from '../../components/job-search/job-search';
@@ -61,7 +61,7 @@ export default function HomePage() {
       `http://localhost:3000/api/users/cv-array/${cv.id}`,
       {
         method: 'DELETE',
-      },
+      }
     );
 
     if (response.ok) {
@@ -92,22 +92,41 @@ export default function HomePage() {
       {cvs.length > 0 &&
         cvs.map((cv: CvFormValues) => {
           return (
-            <CVCard key={cv.id}>
-              <Button type="button" data-textid={cv.cvName} onClick={() => handleClick(cv)}>
-                {cv.cvName}
-              </Button>
-              <Button type="button" onClick={() => deleteCv(cv)}>
-                <svg
-                  height="25"
-                  viewBox="0 0 48 48"
-                  width="25"
-                  xmlns="http://www.w3.org/2000/svg"
+            <CvCard key={cv.id}>
+              <H3>{cv.cvName}</H3>
+              <CvCardButtons>
+                <ButtonSecondary
+                  type="button"
+                  data-textid={cv.cvName}
+                  onClick={() => handleClick(cv)}
                 >
-                  <path d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4v-24h-24v24zm26-30h-7l-2-2h-10l-2 2h-7v4h28v-4z" />
-                  <path d="M0 0h48v48h-48z" fill="none" />
-                </svg>
-              </Button>
-            </CVCard>
+                  <svg
+                    fill={colors.blue}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 384 512"
+                    height={20}
+                    width={20}
+                  >
+                    <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
+                  </svg>
+                </ButtonSecondary>
+                <ButtonSecondary
+                  aria-label="Delete CV"
+                  type="button"
+                  onClick={() => deleteCv(cv)}
+                >
+                  <svg
+                    fill={colors.yellow}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                    height={20}
+                    width={20}
+                  >
+                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                  </svg>
+                </ButtonSecondary>
+              </CvCardButtons>
+            </CvCard>
           );
         })}
       {favJobs.length > 0 && <H2>Your Saved Job Ads</H2>}
@@ -135,11 +154,23 @@ const HomePageArea = styled.div`
   align-items: center;
 `;
 
-const CVCard = styled.div`
+const CvCard = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 5px;
-  width: 200px;
+  align-items: center;
+  padding: 5px 15px;
+  width: 90%;
+  margin-bottom: 15px;
+  max-width: 600px;
   height: 50px;
+  background: ${colors.transparent};
+  color: ${colors.light};
+  border-radius: 15px;
+`;
+
+const CvCardButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
 `;
