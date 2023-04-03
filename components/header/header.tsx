85c2e7b6bf99cc291f58a3150924ba354343a3e4
panisara-bd <panisara.bnn@gmail.com>
2023-04-03 11:52:31 +0200

    Add the back button

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,12 +3,26 @@ import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import { colors, mediaScreen } from '../../helpers/theme';
 import LogInChip from '../log-in-chip';
+import { ButtonSecondary } from '../../helpers/button';
 
 export default function Header() {
   const router = useRouter();
 
+  
   return (
     <HeaderContainer>
+      {router.pathname === '/'? null :
+      <PrevButton onClick={() => router.back()}>
+        <svg
+          fill={colors.purple}
+          height={20}
+          width={20}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" />
+        </svg>
+      </PrevButton>}
       <NavItems>
         <NavList>
           <List isActive={router.pathname === '/'}>
@@ -31,7 +45,11 @@ const HeaderContainer = styled.header`
   align-items: flex-end;
   justify-content: flex-end;
   padding: 40px;
+  margin-inline: 50px;
 
+  @media (max-width: 800px) {
+    margin-inline: 0;
+  }
   @media (max-width: ${mediaScreen.small}) {
     padding: 20px 10px;
   }
@@ -86,7 +104,22 @@ const NavListLink = styled(Link)`
   width: 100%;
   color: ${colors.light};
 
-  &:hover{
-    color: ${colors.purple}
+  &:hover {
+    color: ${colors.purple};
   }
+`;
+
+const PrevButton = styled(ButtonSecondary)`
+display: flex; 
+align-items: center;
+background: ${colors.dark};
+margin-right: 50%;
+
+&:hover {
+  background: ${colors.transparent};
+}
+
+@media (max-width: ${mediaScreen.small}) {
+  display: none;
+}
 `;
